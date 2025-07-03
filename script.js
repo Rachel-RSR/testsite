b2b5da233b3f06431b77751190892053152a4a18
Rachel-RSR <rachel_ramsaran@hotmail.com>
2025-07-03 16:16:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
 let currentSlide = 0;
 
 function showNextSlide() {
   slides[currentSlide].classList.remove('active');
+  dots[currentSlide].classList.remove('active-dot');
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].classList.add('active');
+   dots[currentSlide].classList.add('active-dot');
 }
 
-setInterval(showNextSlide, 3000); // change every 3 seconds
+setInterval(showNextSlide, 3000); 
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    dots[currentSlide].classList.remove('active-dot');
+    currentSlide = index;
+    slides[currentSlide].classList.add('active');
+    dots[currentSlide].classList.add('active-dot');
+  });
+});
